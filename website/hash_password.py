5ece679adc5652b53639a28fa5b4f8df1fc2ddd2
imyvj <youjingumasing@gmail.com>
2025-06-19 02:50:20 +0800

    Added notes - passwords for admins

--- website/hash_password.py
+++ website/hash_password.py
@@ -1,3 +1,8 @@
 from werkzeug.security import generate_password_hash
 
-print(generate_password_hash("your_password"))
+print(generate_password_hash("hotdog"))
+
+# user@example.com - your_password
+# intet01 - 123456
+# gelly02 - 000000
+# youyou - hotdog
